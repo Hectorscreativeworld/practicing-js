2bd77610fafd093f5f5375c3bf32aff97b92dc3c
hectorscreativeworld <hectorscreativeworld@gmail.com>
2019-09-06 17:53:49 -0400

    easy coding challenge

--- public/main.js
+++ public/main.js
@@ -1,7 +1,16 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
-}
+let johnsWeight = 125
+let johnsHeight = 5
+let marksWeight = 165
+let marksHeight = 6
 
-document.addEventListener('DOMContentLoaded', main)
+let johnsBmi = johnsWeight / (johnsHeight * 2)
+johnsBmi = 12.5
+
+let marksBmi = marksWeight / (marksHeight * 2)
+marksBmi = 13.75
+
+let combineBmi = johnsBmi + marksBmi
+combineBmi = 26.25
+
+let highestBmi = marksBmi >= johnsBmi
+console.log("Is Mark's BMI higher than John's?" + highestBmi)
