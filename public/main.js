e82c8dcdf71c873390b8b967b5f082f5986c05b1
hectorscreativeworld <hectorscreativeworld@gmail.com>
2019-09-19 20:42:23 -0400

    set everthing to 0

--- public/main.js
+++ public/main.js
@@ -1,16 +1,76 @@
-let johnsWeight = 125
-let johnsHeight = 5
-let marksWeight = 165
-let marksHeight = 6
+// let johnsWeight = 125
+// let johnsHeight = 5
+// let marksWeight = 165
+// let marksHeight = 6
 
-let johnsBmi = johnsWeight / (johnsHeight * 2)
-johnsBmi = 12.5
+// let johnsBmi = johnsWeight / (johnsHeight * 2)
+// johnsBmi = 12.5
 
-let marksBmi = marksWeight / (marksHeight * 2)
-marksBmi = 13.75
+// let marksBmi = marksWeight / (marksHeight * 2)
+// marksBmi = 13.75
 
-let combineBmi = johnsBmi + marksBmi
-combineBmi = 26.25
+// let combineBmi = johnsBmi + marksBmi
+// combineBmi = 26.25
 
-let highestBmi = marksBmi >= johnsBmi
-console.log("Is Mark's BMI higher than John's?" + highestBmi)
+// let highestBmi = marksBmi >= johnsBmi
+// console.log("Is Mark's BMI higher than John's?" + highestBmi)
+
+// let johnsTeamPoints = (89 + 120 + 103) / 3
+// let mikesTeamPoints = (116 + 94 + 123) / 3
+// console.log(johnsTeamPoints, mikesTeamPoints)
+
+// if (johnsTeamPoints > mikesTeamPoints) {
+//   console.log("John's team wins with " + johnsTeamPoints + 'points')
+// } else if (mikesTeamPoints > johnsTeamPoints) {
+//   console.log("Mike's team wins with " + mikesTeamPoints + 'points')
+// } else {
+//   console.log('There is a draw')
+// }
+
+// for (var i = 0; i < 10; i++) {
+//   console.log(i)
+// }
+
+// var john = ['John', 'Smith', 1990, 'designer', false, 'blue']
+// for (var i = 0; i < john.length; i++) {
+//   console.log(john[i])
+// }
+
+// creating objects and methods
+// let john = {
+//   fullName: 'John Smith',
+//   mass: 92,
+//   Height: 1.95,
+//   calcBmi: function() {
+//     this.bmi = this.mass / (this.Height * 2)
+//     return this.bmi
+//   }
+// }
+
+// let mark = {
+//   fullName: 'Mark Miller',
+//   mass: 78,
+//   Height: 1.69,
+//   calcBmi: function() {
+//     this.bmi = this.mass / (this.Height * 2)
+//     return this.bmi
+//   }
+// }
+
+// john.calcBmi()
+// mark.calcBmi()
+
+// if (john.bmi > mark.bmi) {
+//   console.log(john.fullName + ' ' + 'has a higher BMI of' + ' ' + mark.bmi)
+// } else if (mark.bmi > john.bmi) {
+//   console.log(mark.fullName + ' ' + 'has a higher BMI of' + ' ' + john.bmi)
+// } else {
+//   console.log('They are the same BMI')
+// }
+
+// looping backwards
+
+// let john = ['John', 'Smith', 1990, 'designer', false, 'blue']
+// for (let i = john.length - 1; i >= 0; i--) {
+//   console.log(john[i])
+// }
